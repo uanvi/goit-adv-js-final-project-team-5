b7f68e857e8ec5af0140c08825f90533143036c0
Pavlo <pavlo.rohozhyn@gmail.com>
2025-04-11 22:58:44 +0200

    switch header icons to another sprite (sprites.svg), header links added class active

---
+++ src/js/nav.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.nav-list-link');
+
+  const parts = window.location.pathname
+    .split('/')
+    .filter(s => s && s.length > 0);
+
+  let currentPath;
+  if (parts.length == 0) {
+    currentPath = 'index.html';
+  } else {
+    currentPath = parts[parts.length - 1];
+  }
+
+  navLinks.forEach(link => {
+    if (link.getAttribute('href').endsWith(currentPath)) {
+      link.classList.add('active');
+    }
+  });
+
+  if (!document.querySelector('.nav-link.active')) {
+    navLinks[0].classList.add('active');
+  }
+});
